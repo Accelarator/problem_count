a0b40475473eb298215ac531cd1baa8c7db0053a
tokers <zchao1995@gmail.com>
2016-04-09 22:34:57 +0800

    sgu working

--- core/sgu_count.py
+++ core/sgu_count.py
@@ -4,17 +4,25 @@ from common import spide
 from config import sgu_url 
 from config import get_data_failed
 from config import match_user_failed
+from config import sgu_ac_number_pattern
+from config import sgu_table_pattern
+from config import sgu_tr_pattern
 
 def spide_for_sgu(username, url):
-    data = decompress(spide(url))
+    data = decompress(spide(url+username))
     if data is None:
         return get_data_failed
-    data = data.decode('gb2312')
-    print(data)
+    data = data.decode('utf-8')
+    table_data = match(sgu_table_pattern, data)
+    if table_data is None:
+        return match_user_failed
+    tr_data = match(sgu_tr_pattern, table_data[0])[17]
+    return match(sgu_ac_number_pattern, tr_data)[0]
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) <= 1:
         print("Input your sgu account please")
     else:
-        spide_for_sgu(sys.argv[1], sgu_url)
+        ac_number = spide_for_sgu(sys.argv[1], sgu_url)
+        print(ac_number)
