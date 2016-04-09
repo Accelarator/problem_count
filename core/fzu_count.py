eb02c8d6aab2862e9b84c176af1cf56cf7ec3fa6
tokers <zchao1995@gmail.com>
2016-04-09 22:11:07 +0800

    fzu working

---
+++ core/fzu_count.py
@@ -0,0 +1,23 @@
+from common import decompress
+from common import match
+from common import spide
+from config import fzu_url
+from config import fzu_ac_number_pattern
+
+def spide_for_fzu(username, url):
+    data = decompress(spide(url))
+    if data is None:
+        return get_data_failed
+    data = data.decode('utf-8')
+    ac_number = match(fzu_ac_number_pattern, data)
+    if ac_number is None:
+        return match_user_failed
+    return ac_number[1]
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) <= 1:
+        print('Input your fzu account please.')
+    else:
+        ac_number = spide_for_fzu(sys.argv[1], fzu_url+sys.argv[1])
+        print(ac_number)
